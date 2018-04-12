7c3bd2549bb4a91af83d1d98d0c8558a2e66df8e
devilweime <1665382553@>
2018-04-12 23:09:24 +0800

    Signed-off-by: devilweime <1665382553@>

--- android/android_demo_mix_original/app/src/main/java/com/example/wgw/android_demo_mix_original/MainActivity.java
+++ android/android_demo_mix_original/app/src/main/java/com/example/wgw/android_demo_mix_original/MainActivity.java
@@ -1,14 +1,99 @@
 package com.example.wgw.android_demo_mix_original;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
+
 //https://blog.csdn.net/lantiankongmo/article/details/52302429
 //https://blog.csdn.net/tyyj90/article/details/49700833
 public class MainActivity extends Activity {
 
+
+    Button buttonNull,buttonVar;
+    WebView mWebView;
+
+    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonNull = (Button) findViewById(R.id.button_null);
+        buttonVar = (Button) findViewById(R.id.button_var);
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        buttonNull.setOnClickListener(onClickListener);
+        buttonVar.setOnClickListener(onClickListener);
+
+        WebSettings webSettings = mWebView.getSettings();
+
+        //asset文件夹需带android前缀
+        mWebView.loadUrl("file:///android_asset/inner.html");
+
+        //启用javascript
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+        });  //不设置alert无法弹出
+
+        //设置html的window.android变量
+        mWebView.addJavascriptInterface(MainActivity.this, "android");
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        public void onClick(View view) {
+           switch (view.getId()){
+               case R.id.button_null:
+
+                   //调用javascript的androidCallJs
+                   mWebView.loadUrl("javascript:androidCallJs('我来自Android')");
+                   break;
+               case R.id.button_var:
+                   mWebView.evaluateJavascript("doSum(424121,2)", new ValueCallback<String>() {
+                       @Override
+                       public void onReceiveValue(String valueCallback) {//doSum的结果回调
+                           Log.i("sum", "onReceiveValue:---------> " + valueCallback);
+                           Toast.makeText(MainActivity.this, valueCallback, Toast.LENGTH_SHORT).show();
+                       }
+                   });
+                   break;
+           }
+        }
+    };
+
+
+    //声明为window.android的alertDialog方法
+    @JavascriptInterface
+    public void alertDialog(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new AlertDialog.Builder(MainActivity.this).
+                        setMessage(text).show();
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void onSumResult(int sum) {
+        Log.i("sum", "onSumResult: ------>" + sum);
+        Toast.makeText(MainActivity.this, sum + "",
+                Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ android/android_demo_mix_original/readme.md
@@ -0,0 +1,55 @@
+##混合开发
+    ###前期核心
+
+    ```
+    WebView载入html
+
+    //asset文件夹需带android前缀
+     mWebView.loadUrl("file:///android_asset/inner.html");
+
+     //启用javascript
+     webSettings.setJavaScriptEnabled(true);
+
+     mWebView.setWebChromeClient(new WebChromeClient() {
+     });  //不设置alert无法弹出
+
+     ###Android控件调js
+     ```
+     1.
+      //调用javascript的androidCallJs
+      mWebView.loadUrl("javascript:androidCallJs('我来自Android')");
+
+      或
+      mWebView.evaluateJavascript("doSum(424121,2)", new ValueCallback<String>() {
+        @Override
+        public void onReceiveValue(String valueCallback) {//doSum的结果回调
+        Log.i("sum", "onReceiveValue:---------> " + valueCallback);
+        Toast.makeText(MainActivity.this, valueCallback, Toast.LENGTH_SHORT).show();
+         }
+      });
+
+
+     2.
+     html写好function androidCallJs(text){...}
+
+
+     ###js调安Android后台服务
+     ```
+     1.
+     //设置html的window.android变量
+     mWebView.addJavascriptInterface(MainActivity.this, "android");
+
+     2.
+     //声明为window.android的alertDialog方法
+         @JavascriptInterface
+         public void alertDialog(final String text) {
+             runOnUiThread(new Runnable() {
+                 @Override
+                 public void run() {
+                     new AlertDialog.Builder(MainActivity.this).
+                             setMessage(text).show();
+                 }
+             });
+
+     3.
+     html调用window.android.alertDialog("I am come from JS")
