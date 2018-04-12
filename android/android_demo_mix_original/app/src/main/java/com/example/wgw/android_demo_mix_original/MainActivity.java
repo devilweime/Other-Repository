8e22fe2f27c770735d32fdc37eb650d162f7d9d5
wgw <1665382553@qq.com>
2018-04-12 18:37:54 +0800

    Signed-off-by: wgw <1665382553@qq.com>

--- android/android_demo_mix_original/app/src/main/java/com/example/wgw/android_demo_mix_original/MainActivity.java
+++ android/android_demo_mix_original/app/src/main/java/com/example/wgw/android_demo_mix_original/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.wgw.android_demo_mix_original;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+//https://blog.csdn.net/lantiankongmo/article/details/52302429
+//https://blog.csdn.net/tyyj90/article/details/49700833
 public class MainActivity extends Activity {
 
     @Override
