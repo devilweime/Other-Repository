c2704a4497a75a65a34397d360ec90a3d543754e
wgw <1665382553@qq.com>
2018-04-18 18:32:42 +0800

    Signed-off-by: wgw <1665382553@qq.com>

---
+++ android/android_demo_zxing/app/src/main/java/com/example/wgw/android_demo_zxing/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wgw.android_demo_zxing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+//https://github.com/bingoogolapple/BGAQRCode-Android.git
+//https://github.com/yuzhiqiang1993/zxing/tree/master/app/src/main
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
